e5e9f144b84646cb72fa694619cfd5192dfaeb76
Quogu <Quogu@Quogu>
2012-07-17 20:44:09 +0100

    Made ever so slightly tinier.

--- 2011/bio2011-1a-brevity.hs
+++ 2011/bio2011-1a-brevity.hs
@@ -1,6 +1,6 @@
-c1 $$ c2=toEnum(f(mod(fromEnum c1-64+fromEnum c2-64)26)+64)
+x $$ y=toEnum(f(mod(fromEnum x-64+fromEnum y-64)26)+64)
 f 0=26
 f n=n
-b c1 c2 n|n==1=c1
- |n==2=c2
- |otherwise=b c2(c1 $$ c2)(n-1)
+b x y n|n==1=x
+ |n==2=y
+ |otherwise=b y(x $$ y)(n-1)
